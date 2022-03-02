c8a3bbd476d5a62889d2198c05d8b98a1d3d328e
Brian Rosamilia <brian.rosamilia@gmail.com>
2022-03-02 18:27:37 -0500

    Fixes #11 by not calling native console methods

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const methods = ['log', 'info', 'warn', 'error'];
-const untouchedLogMethods = {};
-methods.forEach(m => untouchedLogMethods[m] = console[m]);
 
 const higherOrderLog = (name, context) => {
-    //Prevents an infinite cycle of reassignment
-    const savedMethod = untouchedLogMethods[name];
-
     const logFn = (...params) => {
-        savedMethod(...params);
-
         if(context[name]){
             context[name](...params);
         }
         else if(context.log[name]){
-            //Must check context.log for some of the methods (currently warn, info, error)
+            //Must check context.log for some methods (currently warn, info, error)
             context.log[name](...params);
         }
     };
